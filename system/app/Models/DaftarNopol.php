b3dda3bc38f541cdfc9f132f5c3e4401bc44a227
ariyanto08 <102232039+ariyanto08@users.noreply.github.com>
2023-11-15 14:47:15 +0700

    .

---
+++ system/app/Models/DaftarNopol.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DaftarNopol extends Model
+{
+    use HasFactory;
+    protected $table = 'saber_daftar_nopol';
+    protected $primatyKey ='nopol_id';
+}
---
+++ system/app/Models/Layanan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Layanan extends Model
+{
+    use HasFactory;
+    protected $table = 'saber_layanan';
+    protected $primaryKey = 'layanan_id';
+}
